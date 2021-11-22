6a667a7c535d0de03748fe525f4556e74b1c83a2
baki_junior <bakirovildar2011@mail.ru>
2021-11-22 17:07:10 +0500

    tesst

---
+++ test.js
@@ -0,0 +1,10 @@
+// var ninja = {
+//     chirp : function (n) {
+//         return n>1 ? this.chirp(n-1) + '-chirp': 'chirp'
+//     }
+// }
+//
+// function getElements(name) {
+//     if (!getElements.cache) getElements.cache = {}
+//     return getElements.cache[name] = getElements.cache[name] || document.getElementsByTagName(name)
+// }//Кэширование DOM элемента
