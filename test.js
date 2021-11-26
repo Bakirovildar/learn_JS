a7b7d5318337b6b3f6254cc13b5ea4a8c902409e
baki_junior <bakirovildar2011@mail.ru>
2021-11-26 16:49:35 +0500

    Типы данных

--- test.js
+++ test.js
@@ -1,10 +1,106 @@
-// var ninja = {
-//     chirp : function (n) {
-//         return n>1 ? this.chirp(n-1) + '-chirp': 'chirp'
+// function a(name) {
+//     return function (age) {
+//         console.log(name,age)
 //     }
 // }
 //
-// function getElements(name) {
-//     if (!getElements.cache) getElements.cache = {}
-//     return getElements.cache[name] = getElements.cache[name] || document.getElementsByTagName(name)
-// }//Кэширование DOM элемента
+// var aa= a('Ildar')
+// aa(25)
+//
+// Рекурсия
+// function timer (n) {
+//     if (n > 60) {
+//         return
+//     }
+//     setTimeout(() => {
+//         console.log(n)
+//         return timer(n+1)
+//     }, 100)
+// }
+//
+// var ff = timer(1)
+//
+// console.log(ff)
+
+
+//Замыкания
+// function nameAge(name) {
+//     return function (age) {
+//         return console.log(name, age)
+//     }
+// }
+//
+// var name1 = nameAge('Ildar')
+// name1(25)
+//
+// var name2 = nameAge('Zarina')
+// name2(20)
+
+
+//Массивы
+// var age = [18,32,25,20,15]
+// var age2 = [22,38]
+// console.log(age)
+//
+// age.push(27)
+// console.log(age)
+//
+// console.log(age.join('-'))
+
+// function list () {
+//     console.log('Сегодня я делал: ')
+//     let length = arguments.length
+//     for (let i = 0; i < length; i++) {
+//         console.log((i+1) + '- ' + arguments[i])
+//     }
+// }
+//
+// list('сходил в больницу', 'сходил в аптеку', 'домой пошел', 'покушал', 'пошел в больницу', 'вырвали зуб', 'сходил к бабушке')
+//
+// function draw (n) {
+//     for (let i = 0; i < n; i++) {
+//         console.log(drawTriangle(i))
+//     }
+//     for (let i = n; i > 0 ; i--) {
+//         console.log(drawTriangle(i))
+//     }
+// }
+//
+// function drawTriangle (n) {
+//     let line = ''
+//     for (let i = 0; i < n; i++) {
+//         line = line + '*'
+//     }
+//     return line
+// }
+//
+// draw(5)
+
+//Типы Данных
+var num = 3
+
+var string = 'hello1'
+
+var boolea = true
+
+var object = {a: 4}
+
+var nul = null
+
+var undefine = undefined
+
+console.log()
+
+//
+// var num1 = 12
+// var num2 = 2
+//
+// var str1 = 'Hello'
+// var str2 = 'World'
+//
+// console.log('5' - '7')
+
+// Приоритеты
+// var vv = 5 + 5 * 5 > 40
+//
+// console.log(vv)
