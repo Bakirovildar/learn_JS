4187f356176affde0d2b3279f48cb41ac6d09680
baki_junior <bakirovildar2011@mail.ru>
2021-11-27 12:09:52 +0500

    Логические операторы && и ||. Пример с if, else if, else

--- test.js
+++ test.js
@@ -1,21 +1,18 @@
-console.log(true && true)
+var carName = 'Ford'
+var carYear = 2012
+var year = 2021 - carYear
+if (year < 5) {
+    console.log('меньше пяти лет')
+} else if (year > 8 && year <= 10 ) {
+    console.log('больше восьми и меньше 10 ти')
+} else {
+    console.log('средних лет')
+}
 
-console.log(true && false)
+var str = 'Ford'
 
-console.log(false && true)
-
-console.log(false || true)
-
-console.log(true || true)
-
-console.log(false || false)
-
-console.log(!true)
-
-console.log(!!true)
-
-console.log(!!false)
-
-console.log((true && false) || (true || false) || !true)
-
-console.log(false || false || true || false || false || false)
+if (str) {
+    console.log('poluch true')
+} else {
+    console.log('poluch false')
+}
