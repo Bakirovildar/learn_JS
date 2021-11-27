4c9293f5b1a7754f297b85558a3719fe22ae0be9
baki_junior <bakirovildar2011@mail.ru>
2021-11-27 12:27:38 +0500

    Тернарные операторы ?, :

--- test.js
+++ test.js
@@ -1,18 +1,8 @@
-var carName = 'Ford'
-var carYear = 2012
-var year = 2021 - carYear
-if (year < 5) {
-    console.log('меньше пяти лет')
-} else if (year > 8 && year <= 10 ) {
-    console.log('больше восьми и меньше 10 ти')
-} else {
-    console.log('средних лет')
-}
+var personAge = 17
+var message
 
-var str = 'Ford'
+message = personAge < 18
+    ? 'Человек несовершеннолетний'
+    : 'Человек совершеннолетний'
 
-if (str) {
-    console.log('poluch true')
-} else {
-    console.log('poluch false')
-}
+console.log(message)
