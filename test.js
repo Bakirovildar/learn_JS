ddfb7fdc9351320534bb14b6e7d0b19366ae208d
baki_junior <bakirovildar2011@mail.ru>
2021-11-27 10:25:09 +0500

    Логические операторы && и ||.

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-function f(arg) {
-    console.log('Что я сделал за сегодня: ')
-    let length = arguments.length
-    for (let i = 0; i < length ; i++) {
-        console.log(i + '- ' + arguments[i])
-    }
-}
+console.log(true && true)
 
-f('Поехал в город', 'Написал заявление на расторжение договора страховки', 'Сходил в баню')
+console.log(true && false)
+
+console.log(false && true)
+
+console.log(false || true)
+
+console.log(true || true)
+
+console.log(false || false)
