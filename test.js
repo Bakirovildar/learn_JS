659e1075e691f498d5bfb7e4038f0aabff171ce5
baki_junior <bakirovildar2011@mail.ru>
2021-11-28 13:32:09 +0500

    методы строк

--- test.js
+++ test.js
@@ -1,15 +1,11 @@
-var carColor = 'yellow'
+var personName = 'Victor Classon!!!'
 
-switch (carColor) {
-    case "yellow":
-        console.log('желтый')
-        break
-    case 'red':
-        console.log('red')
-        break
-    case 'green':
-        console.log('green')
-        break
-    default:
-        console.log('Неопределен')
-}
+console.log(personName.toUpperCase())// Все с большими
+console.log(personName.toLowerCase()) // Все с маленькими
+console.log(personName.charAt(0)) // Выводит 0 индекс т.е букву 'V'
+console.log(personName.indexOf('o')) // 4 Так как "о" по индексу 4
+console.log(personName.indexOf('ro')) // -1 Если нет результата
+console.log(personName.substr(1,2)) // Выводит в интервале 2 включительно
+console.log(personName.substr(personName.indexOf('Classon'),7)) // Также, только начало через .indexOf
+console.log(personName.substring(0,2)) // 2 не включительно
+
