e70a0aae81075ea1af328c312888bf35250ca3ab
baki_junior <bakirovildar2011@mail.ru>
2021-11-28 15:17:41 +0500

    Массивы

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
-var personName = 'Victor Classon!!!'
+var cars = ['Audi','BMW','Mazda','Kia',] //Массив
 
-console.log(personName.toUpperCase())// Все с большими
-console.log(personName.toLowerCase()) // Все с маленькими
-console.log(personName.charAt(0)) // Выводит 0 индекс т.е букву 'V'
-console.log(personName.indexOf('o')) // 4 Так как "о" по индексу 4
-console.log(personName.indexOf('ro')) // -1 Если нет результата
-console.log(personName.substr(1,2)) // Выводит в интервале 2 включительно
-console.log(personName.substr(personName.indexOf('Classon'),7)) // Также, только начало через .indexOf
-console.log(personName.substring(0,2)) // 2 не включительно
+cars.push('Mercedes') // Добавляет новый элемент в КОНЕЦ массива
+cars.unshift('Skoda') // Добавляет новый элемент в НАЧАЛО массива
+cars.pop() // Удаляет последний элемент в массиве
+cars.shift() // Удаляет первый элемент в массиве
 
+var BMW =  cars.indexOf('BMW')// Находит индек по имени 'BMW'
