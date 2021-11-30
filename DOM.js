8940eb07ff9579cee234ec816643c025f7bf1b25
baki_junior <bakirovildar2011@mail.ru>
2021-11-30 17:32:33 +0500

    DOM. addEventListener. mouseenter, mouseleave, click. События

--- DOM.js
+++ DOM.js
@@ -1,15 +1,16 @@
+var h1 = document.querySelector('h1')
+var input = document.querySelector('input')
+var button = document.querySelector('button')
+var body = document.querySelector('body')
 
-//DOM
+body.addEventListener("mouseenter", function () {
+    body.style.backgroundColor = 'aqua'
+})
 
-//Обращение к тегам, классам и id:
+body.addEventListener("mouseleave", function () {
+    body.style.backgroundColor = 'blue'
+})
 
-var h1 = document.querySelector('h1') // элемент
-var p = document.querySelector('p') // элемент
-var names = document.querySelectorAll('.sity') // .класс
-var red = document.querySelector('#red') // #id
-var s = document.querySelector('.y > .ss > ul') // ul дочерний к .ss, а .ss к .y
-
-//Добавить и изменить
-
-s.innerHTML = '<h1 style="color: aquamarine"> Learn JavaScript</h1>'// innerHTML добавить что то
-h1.textContent = 'DOM (Document Object Model)'  // Изменил h1 c .textContent
+button.addEventListener("click", function () {
+    h1.textContent = input.value
+})
