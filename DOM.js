9e85c6b9e964b8f1ddb7ce5d1fcbd788bee832ce
baki_junior <bakirovildar2011@mail.ru>
2021-12-03 11:42:57 +0500

    Math.random(), Math.floor(), Math.ceil()

--- DOM.js
+++ DOM.js
@@ -1,20 +1,4 @@
-var divs = document.querySelectorAll('div')
-var link = document.querySelector('a')
-
-for (let i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', function () {
-        event.stopPropagation() // ОСТАНАВЛИВАЕТ ОБРАБОТКУ
-        console.log(divs[i].getAttribute('id'))
-    }, )
-
-}
-
-link.addEventListener("click", function () { // ПО КЛИКУ
-    event.preventDefault() // ССЫЛКА НЕ ПЕРЕХОДИТ
-
-    var div1 = divs[0]
-
-   div1.style.display = div1.style.display === 'none'
-       ? 'flex'
-       : 'none'
-})
+var num = 2.5
+Math.random()
+Math.floor(num) // Округляет в меньшую сторону
+Math.ceil(num) //Округляет в большую сторону
