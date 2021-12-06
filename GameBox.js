baacfadc7efb247b49619ec8241eb8d02b24dd47
baki_junior <bakirovildar2011@mail.ru>
2021-12-06 20:31:14 +0500

    Приложение на JS 'Игра в квдратики1'

--- GameBox.js
+++ GameBox.js
@@ -1,5 +1,5 @@
-var $game = document.querySelector('#game')
 var $start = document.querySelector('#start')
+var $game = document.querySelector('#game')
 var $time = document.querySelector('#time')
 var $timeHader = document.querySelector('#time-header')
 var $resultHader = document.querySelector('#result-header')
@@ -7,34 +7,32 @@ var $result = document.querySelector('#result')
 var $gameTime = document.querySelector('#game-time')
 
 var score = 0
-var isGameStarted = false
-var colors = ['violet', 'aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 'silver', 'teal', 'yellow']
-
+var isGameStart = false
 
 $start.addEventListener('click', startGame)
-$game.addEventListener('click', handleBoxClick)
+$game.addEventListener('click', clickGameBox)
 $gameTime.addEventListener('input', setTime)
 
 function startGame() {
     score = 0
-    setTime()
-    $gameTime.setAttribute('disabled', 'true')
-    show($timeHader)
-    hide($resultHader)
-    isGameStarted = true
     $game.style.background = '#fff'
     hide($start)
+    setTime()
+    $gameTime.setAttribute('disabled', 'true')
+    isGameStart = true
+    show($timeHader)
+    hide($resultHader)
+
+
     var interval = setInterval(function () {
-        var time = parseFloat($time.textContent)
+        var time = $time.textContent
 
         if (time <= 0) {
             clearInterval(interval)
             endGame()
-
         } else {
-            $time.textContent = (time - 0.1).toFixed(1)
+            $time.textContent = (time - 0.100).toFixed(1)
         }
-
     }, 100)
 
     renderBox()
@@ -42,7 +40,27 @@ function startGame() {
 
 function setTime() {
     var time = +$gameTime.value
-    $time.textContent = time.toFixed(1)
+    $time.textContent = time
+}
+
+function clickGameBox(event) {
+    if (event.target.getAttribute('data-box')) {
+        score++
+        renderBox()
+    }
+}
+
+function endGame() {
+    $game.style.background = '#ccc'
+    show($start)
+    isGameStart = false
+    $game.innerHTML = ''
+    hide($timeHader)
+    show($resultHader)
+    $result.textContent = score
+    $gameTime.removeAttribute('disabled')
+
+
 }
 
 function show($el) {
@@ -53,53 +71,27 @@ function hide($el) {
     $el.classList.add('hide')
 }
 
-function setResult() {
-    $result.textContent = score.toString()
-}
-
-function endGame() {
-    isGameStarted = false
-    $gameTime.removeAttribute('disabled')
-    show($start)
-    $game.style.background = '#ccc'
-    $game.innerHTML = ''
-    hide($timeHader)
-    show($resultHader)
-    setResult()
-
-}
-
-function handleBoxClick(event) {
-    if (!isGameStarted) {
+function renderBox() {
+    if (!isGameStart) {
         return
     }
-    if (event.target.getAttribute('data-box')) {
-        score++
-        renderBox()
-    }
-}
-
-function renderBox() {
-
     $game.innerHTML = ''
     var box = document.createElement('div')
     var sizeBox = getRandom(30, 100)
-    var gameBox = $game.getBoundingClientRect()
-    var maxTop = gameBox.height - sizeBox
-    var maxLeft = gameBox.width - sizeBox
-    var colorRand = getRandom(0, colors.length - 1)
+    var gameParams = $game.getBoundingClientRect()
+    var maxTop = getRandom(0, gameParams.height - sizeBox)
+    var maxLeft = getRandom(0, gameParams.width - sizeBox)
 
-    box.style.width = box.style.height = sizeBox + 'px'
-    box.style.background = colors[colorRand]
-    box.style.top = getRandom(0, maxTop) + 'px'
-    box.style.left = getRandom(0, maxLeft) + 'px'
+    box.style.height = box.style.width = sizeBox + 'px'
+    box.style.background = '#000'
     box.style.position = 'absolute'
     box.style.cursor = 'pointer'
+    box.style.left = maxLeft + 'px'
+    box.style.top = maxTop + 'px'
     box.setAttribute('data-box', 'true')
 
-    $game.insertAdjacentElement("afterbegin", box)
+    $game.insertAdjacentElement('afterbegin', box)
 }
-
 
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
