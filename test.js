6e25da4aa5e3a0ac3194301fa8b812ff786b0fea
baki_junior <bakirovildar2011@mail.ru>
2021-12-08 11:17:48 +0500

    Object.keys // Иттерация по ключам объекта

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-
-for (i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 !== 0) {
-        continue
-    }
-    console.log(numbers[i])
+var person = {
+    name: 'Ildar',
+    age: 25,
+    job: 'Frontend'
 }
+
+// for (var key in person) {
+//     if (person.hasOwnProperty(key)) // Проверка. Если нету прототипа то вывод ключа
+//     console.log(person[key])
+// }
+
+Object.keys(person).forEach(function (key) {
+    console.log(person[key])  // не пробегается по Прототипам
+})
